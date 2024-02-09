9082b7d268d29a4f75e01799c9ce8771723dc1dd
SimonG <saguti15@g.holycross.edu>
2024-02-09 15:13:23 -0500

    SG: updated users portfolio to be a functional component

--- src/components/portfolio/UsersPortfolioPage.js
+++ src/components/portfolio/UsersPortfolioPage.js
@@ -1,55 +1,52 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { getPortfolioThunkCreator } from '../../store/reducers/portfolioReducer';
 import TableHeader from '../Tables/Table-Header';
 import TableBody from '../Tables/Table-Body';
 
-class UsersPortfolioPage extends Component {
+  function UsersPortfolioPage ({auth, usersPortfolio, getPortfolio}) {
  
-  componentDidMount() {
-     this.props.getPortfolio(this.props.auth.uid);
-  }
-
-  findTotalValue() {
-    let totalValue = 0;
-    this.props.usersPortfolio.forEach(elem => totalValue += elem.currentValue);
-
-    return totalValue;
-  }
-    
-    render () {
+    useEffect(() => getPortfolio(auth.uid), [auth.uid, getPortfolio]);
+  
+    const findTotalValue = () => {
+      let totalValue = 0;
+      usersPortfolio.forEach(elem => totalValue += elem.currentValue);
+  
+      return totalValue;
+    }
+      
+     
         let totalValue;
-        this.props.usersPortfolio.length ? totalValue = this.findTotalValue() : totalValue = 0;
-
-      return (
-        <div className="section">
-          <div className="card z-depth-0">
-            <div className="card-content grey-text text-darken-3">
-              <h1 className='title'>My Portfolio (${Number.parseFloat(totalValue).toFixed(2)})</h1>
-              <table className='transactions'>
-                <tbody>
-                  { 
-                    this.props.usersPortfolio.length ?
-                    <TableHeader headers={this.props.usersPortfolio[0]} /> : 
-                    <tr>
-                      <th>No Stock Owned Yet</th>
+        usersPortfolio.length ? totalValue = findTotalValue() : totalValue = 0;
+  
+        return (
+          <div className="section">
+            <div className="card z-depth-0">
+              <div className="card-content grey-text text-darken-3">
+                <h1 className='title'>My Portfolio (${Number.parseFloat(totalValue).toFixed(2)})</h1>
+                <table className='transactions'>
+                  <tbody>
+                    { 
+                      usersPortfolio.length ?
+                      <TableHeader headers={usersPortfolio[0]} /> : 
+                      <tr>
+                        <th>No Stock Owned Yet</th>
+                        </tr>
+                    }
+                    {
+                      usersPortfolio.length ?
+                      <TableBody body={usersPortfolio} /> :
+                      <tr>
+                        <td>Buy some Stock!</td>
                       </tr>
-                  }
-                  {
-                    this.props.usersPortfolio.length ?
-                    <TableBody body={this.props.usersPortfolio} /> :
-                    <tr>
-                      <td>Buy some Stock!</td>
-                    </tr>
-                  }
-                </tbody>
-              </table>
+                    }
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
-        </div>
-      );
-    };
-  }
+        );
+      };
   
 const mapStateToProps = state => ({
     auth: state.firebase.auth,
